2da96cb36b8fb4619eb9e7c095c71ee20e7bd481
Santiago Sánchez <sasanchezramirez@gmail.com>
2023-05-23 21:01:04 -0500

    Finishing datails about connection with database

--- src/main/java/com/task/taskManager/infrastructure/persistencia/entities/TaskEntity.java
+++ src/main/java/com/task/taskManager/infrastructure/persistencia/entities/TaskEntity.java
@@ -1,21 +1,33 @@
 package com.task.taskManager.infrastructure.persistencia.entities;
 
+import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
 @Getter
 @Setter
+@Entity
 @Table("task")
 public class TaskEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String title;
     private String description;
     private Boolean completed;
-
-    public TaskEntity(Integer id, String title, String description, Boolean completed) {
-
+    public TaskEntity() {
     }
+    public TaskEntity(Integer id, String title, String description, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
 }
